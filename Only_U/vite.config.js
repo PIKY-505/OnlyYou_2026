1c2c1ea6f2c6eb7f9ab6a17e47e53a966de7ecb1
PIKY-505 <apiquerasroldan@gmail.com>
2026-01-21 20:52:23 +0000

    -

--- Only_U/vite.config.js
+++ Only_U/vite.config.js
@@ -4,22 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // CAMBIO AQUÍ: Subimos a 1600 kbs (1.6 MB) para cubrir tu vendor de 1.14 MB
-    chunkSizeWarningLimit: 1600,
+  // --- CAMBIO CLAVE ---
+  // Usamos './' para que funcione en CUALQUIER carpeta o repositorio automáticamente.
+  base: "./",
 
+  build: {
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            // Mantenemos la lógica que YA FUNCIONA bien:
-            if (id.includes("ogl")) {
-              return "ogl";
-            }
-            if (id.includes("framer-motion") || id.includes("motion")) {
+            if (id.includes("ogl")) return "ogl";
+            if (id.includes("framer-motion") || id.includes("motion"))
               return "framer-motion";
-            }
-
             return "vendor";
           }
         },
